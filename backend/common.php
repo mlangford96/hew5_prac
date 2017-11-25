7f44468b42a6458dcb78f713ca52096ceea200f7
Matthew Langford <matthew.langford96@gmail.com>
2017-11-26 09:49:29 +1000

    Added functionality for profile upload.

--- backend/common.php
+++ backend/common.php
@@ -1,10 +1,15 @@
 <?php
 
-function connect_to_database() {
-    $path = $_SERVER['DOCUMENT_ROOT'] . '../backend/data.db';
-    $file = 'sqlite:'.path;
-    $conn = new PDO($file) or die("Database Error");
-    return $conn;
-}
+    function connect_to_database() {
+        $path = 'database.db';
+        $file = 'sqlite:' . $path;
+        $conn = new PDO($file) or die("Database Error");
+        return $conn;
+    }
 
+    function add_profile($firstName, $lastName, $jobTitle, $email, $bio, $pathToPicture) {
+        $conn = connect_to_database();
+        $query = $conn->prepare("INSERT INTO PROFILES (FIRSTNAME, LASTNAME, JOBTITLE, EMAIL, BIO, PROFILEPIC) VALUES (?,?,?,?,?,?)");
+        $query->execute(array($firstName, $lastName, $jobTitle, $email, $bio, $pathToPicture));
+    }
 ?>
